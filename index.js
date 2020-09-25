aeeaf98a3352ff7f541144a6f825fe0e418d0fc0
zdzislaw <belazdzislaw@gmail.com>
2020-09-25 23:08:16 +0200

    fixing positive/negative number selector

--- index.js
+++ index.js
@@ -25,21 +25,76 @@ const addKeyListiners = () => {
     })
 }
 
-const setKeyClassInterval = () => {
-    const keys = document.getElementById('keyPanel').childNodes;
+const addMathKeyListiners = () => {
+    const keys = document.querySelectorAll('.mathKey');
+    console.log(keys)
     keys.forEach((key) => {
-        key.classList = "key"
-    });
+        key.addEventListener('mouseover', () => {
+            key.classList = "mouseover"
+        })
+    })
+    keys.forEach((key) => {
+        key.addEventListener('mouseout', () => {
+            key.classList = "key"
+        })
+    })
+    keys.forEach((key) => {
+        key.addEventListener('mousedown', () => {
+            key.classList = "mousedown"
+        })
+    })
+    keys.forEach((key) => {
+        key.addEventListener('mouseup', () => {
+            key.classList = "mouseover"
+            handleMathKey(key.innerHTML)
+        })
+    })
 }
 
+const handleMathKey = (pressedKey) => {
+    const newDisplayRow = document.createElement('div');
+    newDisplayRow.setAttribute('class', 'displayRow');
+    const newDisplaySign = document.createElement('div');
+    newDisplaySign.setAttribute('class', 'displaySign');
+    newDisplaySign.innerHTML = pressedKey;
+
+    if ( pressedKey == '+' ) {
+        document.querySelector('#display').appendChild(newDisplaySign);
+        document.querySelector('.displaySign').innerHTML = "+";
+        document.querySelector('#display').appendChild(newDisplayRow);
+        
+        return updeConsole('missing implementation of "+"');
+    }
+    if ( pressedKey == '-' ) {
+        document.querySelector('.displaySign').innerHTML = "-"
+        return updateConsole('missing implementation of "-"');
+    }
+    if ( pressedKey == '/' ) {
+        document.querySelector('.displaySign').innerHTML = "/"
+        return updateConsole('missing implementation of "/"');
+    }
+    if ( pressedKey == 'x' ) {
+        document.querySelector('.displaySign').innerHTML = "x"
+        return updateConsole('missing implementation of "x"');
+    }
+    if ( pressedKey == '=' ) {
+        document.querySelector('.displaySign').innerHTML = ""
+        return updateConsole('missing implementation of "="');
+    }
+}
 const countingValue = (pressedKey) => {
     updateConsole('');
     console.log("----- Starting countingValue -----");
     console.log("----- User typed: " + pressedKey);
 
     const newValue = pressedKey;
-    const previousValue = document.querySelector('#displayRow').innerHTML;
-    const previousValueArray = Array.from(document.querySelector('#displayRow').innerHTML);
+
+    const row = document.querySelectorAll('.displayRow');
+    const lastRow = row[row.length-1];
+    
+
+    const previousValue = lastRow.innerHTML;
+    const previousValueArray = Array.from(lastRow.innerHTML);
     const lastCharPreviousValue = previousValueArray[previousValueArray.length - 1];
 
     console.log("");
@@ -51,44 +106,21 @@ const countingValue = (pressedKey) => {
 
 
     if ( pressedKey == 'CE' ) {
-        document.querySelector('#sign').innerHTML = ""
+        document.querySelector('.displaySign').innerHTML = ""
         return updateDisplay('0');
     }
     if ( pressedKey == 'C' ) {
-        document.querySelector('#sign').innerHTML = ""
+        document.querySelector('.displaySign').innerHTML = ""
         return updateConsole('missing implementation of "C"');
     }
     if ( pressedKey == 'del' ) {
-        document.querySelector('#sign').innerHTML = ""
+        document.querySelector('.displaySign').innerHTML = ""
         if ( previousValue == '0' | previousValue.length == 1 ) {
             return updateDisplay('0');
         } 
         return updateDisplay( previousValue.slice(0, previousValue.length-1) );
     }
 
-
-    if ( pressedKey == '+' ) {
-        document.querySelector('#sign').innerHTML = "+"
-        return updateConsole('missing implementation of "+"');
-    }
-    if ( pressedKey == '-' ) {
-        document.querySelector('#sign').innerHTML = "-"
-        return updateConsole('missing implementation of "-"');
-    }
-    if ( pressedKey == '/' ) {
-        document.querySelector('#sign').innerHTML = "/"
-        return updateConsole('missing implementation of "/"');
-    }
-    if ( pressedKey == 'x' ) {
-        document.querySelector('#sign').innerHTML = "x"
-        return updateConsole('missing implementation of "x"');
-    }
-    if ( pressedKey == '=' ) {
-        document.querySelector('#sign').innerHTML = ""
-        return updateConsole('missing implementation of "="');
-    }
-
-    
     if ( pressedKey == "+/-" ) {
         if (previousValueArray[0] == '-') {
             return updateDisplay(previousValue.slice(1,previousValue.length));
@@ -119,19 +151,32 @@ const countingValue = (pressedKey) => {
     } 
 }
 
+
+
+
 const updateDisplay = (value) => {
     if (value.length > 15) {
         return updateConsole("I can't handle more sorry");
     } 
-    document.querySelector('#displayRow').innerHTML = value;
+    const row = document.querySelectorAll('.displayRow');
+    const lastRow = row[row.length-1];
+    lastRow.innerHTML = value;
+/*     console.log(" hej tu");
+    console.log(lastRow); */
 }
 const updateConsole = (value) => {
-    console.log("co jest" + value)
+
     document.querySelector('#console').innerHTML = value;
 }
-
+const setKeyClassInterval = () => {
+    const keys = document.querySelectorAll('.key');
+    keys.forEach((key) => {
+        key.classList = "key"
+    });
+}
 const loadApp = () => {
     addKeyListiners();
+    addMathKeyListiners();
     setInterval(setKeyClassInterval, 3000);
 }
 loadApp();
