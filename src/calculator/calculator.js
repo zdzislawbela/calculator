228a4f2a06a71bbbcf6f70e4f7504c9fff522bc3
zdzislaw <belazdzislaw@gmail.com>
2020-10-06 15:50:45 +0200

    Add keyboard component

--- src/calculator/calculator.js
+++ src/calculator/calculator.js
@@ -1,6 +1,7 @@
 import classes from './calculator.css';
 import title from './title/title.js';
 import display from './display/display.js';
+import keyboard from './keyboard/keyboard';
 const createCalculator = () => {
     const calculator = document.createElement('div');
     calculator.setAttribute('class', 'calculator');    
@@ -12,7 +13,5 @@ export default () => {
     createCalculator();
     title();
     display();
-
- /*    keyPanel(); */
-
+    keyboard();
 }
