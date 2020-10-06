9b6a5d6bcbb08f0bbf6703c3b8f2026fd1654a6f
zdzislaw <belazdzislaw@gmail.com>
2020-10-06 13:06:29 +0200

    Compress loops

--- src/keyListeners.js
+++ src/keyListeners.js
@@ -6,18 +6,12 @@ function addKeyListeners() {
         key.addEventListener('mouseover', () => {
             key.classList = "mouseover"
         })
-    })
-    keys.forEach((key) => {
         key.addEventListener('mouseout', () => {
             key.classList = "key"
         })
-    })
-    keys.forEach((key) => {
         key.addEventListener('mousedown', () => {
             key.classList = "mousedown"
         })
-    })
-    keys.forEach((key) => {
         key.addEventListener('mouseup', () => {
             key.classList = "mouseover"
             countingValue(key.innerHTML);
@@ -31,18 +25,12 @@ function addMathKeyListeners() {
         key.addEventListener('mouseover', () => {
             key.classList = "mouseover"
         })
-    })
-    keys.forEach((key) => {
         key.addEventListener('mouseout', () => {
             key.classList = "key"
         })
-    })
-    keys.forEach((key) => {
         key.addEventListener('mousedown', () => {
             key.classList = "mousedown"
         })
-    })
-    keys.forEach((key) => {
         key.addEventListener('mouseup', () => {
             key.classList = "mouseover"
             handleMathKey(key.innerHTML);
