12dce70be83d139227cef77b385c8d8d976e725d
zdzislawbela <47990852+zdzislawbela@users.noreply.github.com>
2021-01-14 00:53:15 +0100

    Update events/handlePressedButton.js
    Co-authored-by: raqso <raqsasg@gmail.com>

--- events/handlePressedButton.js
+++ events/handlePressedButton.js
@@ -1,6 +1,6 @@
 import { updateDisplay } from './displayEvents.js';
 
-const numbers = ['0','1','2','3','4','5','6','7','8','9'];
+const possibleDigits = ['0','1','2','3','4','5','6','7','8','9'];
 const numbersModifiers = ['+/-','.'];
 const deletationSigns = ['CE','C','del'];
 const operators = ['/','x','-','+','='];
