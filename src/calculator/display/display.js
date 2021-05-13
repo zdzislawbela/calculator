3fa567de7448c861e386e93cae7152e8a29a2760
blady <belazdzislaw@gmail.com>
2021-05-13 22:17:45 +0200

    Amend indents

--- src/calculator/display/display.js
+++ src/calculator/display/display.js
@@ -1,17 +1,18 @@
-import classes from './display.css';
+import classes from "./display.css";
 
 const createDisplay = () => {
-    const display = document.createElement('div');
-    const displayRow = document.createElement('div');
-    display.setAttribute('class', 'display');    
-    displayRow.setAttribute('class', 'displayRow');    
-    displayRow.innerHTML = "0";
-    display.appendChild(displayRow);
-    document.querySelector('.calculator').appendChild(display);
+  const display = document.createElement("div");
+  display.setAttribute("class", "display");
 
-    
-}
+  const displayRow = document.createElement("div");
+  displayRow.setAttribute("class", "displayRow");
+  displayRow.innerHTML = "0";
+
+  display.appendChild(displayRow);
+
+  document.querySelector(".calculator").appendChild(display);
+};
 
 export default () => {
-    createDisplay();
-}
+  createDisplay();
+};
